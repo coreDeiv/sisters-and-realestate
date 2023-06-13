2ec06656c57aca2828842cc3e0a00a550400baf8
coredeiv <dftp93@gmail.com>
2023-06-13 16:51:18 -0500

    [13062301] Update home page

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@ window.addEventListener('DOMContentLoaded', function() {
   var body = document.querySelector('body');
   body.id = 'sre-home';
 });
+
+function openTab(event, tabId) {
+  var i, tabcontent, tablinks;
+  // Hide all tabs
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  // delete active class from the button tabs
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  // Show content in the tab selected
+  document.getElementById(tabId).style.display = "flex";
+  event.currentTarget.className += " active";
+}
